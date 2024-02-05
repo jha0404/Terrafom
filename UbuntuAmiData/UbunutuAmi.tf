a880c6cbd1d6d27351195e143be7719379e15eb5
jha0404 <abhj815@gmail.com>
2024-02-05 17:12:45 +0530

    codebase of ubuntu ami data pulling

---
+++ UbuntuAmiData/UbunutuAmi.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "ubuntu22" {
+  most_recent = true
+  owners      = ["amazon"]
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+output "test" {
+  value = data.aws_ami.ubuntu22.boot_mode
+}
